6f54ec5eb06e9a6c856584191f1a2af9aa7b33db
Jakub Kilar <kuba.kilar@gmail.com>
2018-02-25 09:08:56 +0100

    change script - add else if for 0

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ var b = prompt('Podaj b');
 var value = (a * a) - (2 * a * b) - (b * b);
 console.log('a = ' + a);
 console.log('b = ' + b);
-console.log('(a * a) - (2 * a * b) - (b * b)' + ' = ' + value);
+console.log('(' + a + ' * ' + a + ')' + ' - ' + '(' + '2 ' + '* '  + a + ' * ' + b + ')' + ' - ' + '(' + b + ' * ' + b + ') = ' + value);
 
 if (value > 0) {
   console.log('wynik dodatni');
+} else if (value === 0) {
+  console.log('zero');
 } else {
   console.log('wynik ujemny');
 }
