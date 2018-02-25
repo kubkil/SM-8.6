7f7d15948ac34279f8c03c1bc2b9f0da0b819de7
Jakub Kilar <kuba.kilar@gmail.com>
2018-02-25 14:08:42 +0100

    modify calc script

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-var a = prompt('Podaj a');
-var b = prompt('Podaj b');
-var value = (a * a) - (2 * a * b) - (b * b);
+let a = prompt('Podaj a');
+let b = prompt('Podaj b');
+let value = (a * a) - (2 * a * b) - (b * b);
 console.log('a = ' + a);
 console.log('b = ' + b);
-console.log('(' + a + ' * ' + a + ')' + ' - ' + '(' + '2 ' + '* '  + a + ' * ' + b + ')' + ' - ' + '(' + b + ' * ' + b + ') = ' + value);
 
-if (value > 0) {
-  console.log('wynik dodatni');
-} else if (value === 0) {
-  console.log('zero');
-} else {
-  console.log('wynik ujemny');
-}
+// console.log('(' + a + ' * ' + a + ')' + ' - ' + '(' + '2 ' + '* '  + a + ' * ' + b + ')' + ' - ' + '(' + b + ' * ' + b + ') = ' + value);
+
+console.log(`(${a} * ${a}) - (2 * ${a} * ${b}) - (${b} * ${b}) = ${value}`);
+
+// if (value > 0) {
+//   console.log('wynik dodatni');
+// } else if (value === 0) {
+//   console.log('zero');
+// } else {
+//   console.log('wynik ujemny');
+// }
+
+let result = (value > 0) ? 'wynik dodatni':
+  (value < 0) ? 'wynik ujemny':
+  (a.length == 0) || (b.length == 0) ? 'nie podałeś wartości':
+  'zero';
+
+console.log(result);
