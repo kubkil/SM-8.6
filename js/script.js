f1f6a5deaf74d6d16283c8a185c07fb02874531b
kubkil <33201356+kubkil@users.noreply.github.com>
2018-03-01 22:53:51 +0100

    refactor js

--- js/script.js
+++ js/script.js
@@ -8,17 +8,17 @@ console.log('b = ' + b);
 
 console.log(`(${a} * ${a}) - (2 * ${a} * ${b}) - (${b} * ${b}) = ${value}`);
 
-// if (value > 0) {
-//   console.log('wynik dodatni');
-// } else if (value === 0) {
-//   console.log('zero');
-// } else {
-//   console.log('wynik ujemny');
-// }
+if (value > 0) {
+  console.log('wynik dodatni');
+} else if (value === 0) {
+  console.log('zero');
+} else {
+  console.log('wynik ujemny');
+}
 
-let result = (value > 0) ? 'wynik dodatni':
-  (value < 0) ? 'wynik ujemny':
-  (a.length == 0) || (b.length == 0) ? 'nie podałeś wartości a i b':
-  'zero';
+// let result = (value > 0) ? 'wynik dodatni':
+//   (value < 0) ? 'wynik ujemny':
+//   (a.length == 0) || (b.length == 0) ? 'nie podałeś wartości a i b':
+//   'zero';
 
 console.log(result);
